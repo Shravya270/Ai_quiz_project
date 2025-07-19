fb46c83a7ffcdf53f83aad644374caecff5f9866
Shravya270 <shravyashetty246@gmail.com>
2025-07-19 22:43:09 +0530

    Completed the quiz component

--- src/app/quizz/page.tsx
+++ src/app/quizz/page.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import ProgressBar from "@/components/progressBar";
 import {ChevronLeft ,X } from "lucide-react"
 import ResultCard from "./ResultCard";
+import QuizSubmission from "./QuizzSubmission";
 
 
 const questions = [
@@ -54,6 +55,7 @@ export default function Home() {
     const [score,setScore] = useState<number>(0);
     const [selectedAnswer,setSelectedAnswer] = useState<number|null>(null);
     const [isCorrect,setIsCorrect] = useState<boolean|null>(null);
+    const [submitted,setSubmitted] = useState<boolean>(false);
 
     const handleNext =()=>{
         if(!started){
@@ -63,6 +65,9 @@ export default function Home() {
 
         if(currentQuestion<questions.length - 1){
             setCurrentQuestion(currentQuestion+1);
+        }else{
+          setSubmitted(true);
+          return;
         }
 
         setSelectedAnswer(null);
@@ -78,13 +83,25 @@ export default function Home() {
         setIsCorrect(isCurrentCorrect);
     }
 
+    const scorePercentage: number = Math.round((score/questions.length)*100);
+
+    if(submitted){
+      return(
+        <QuizSubmission
+          score={score}
+          scorePercentage={scorePercentage}
+          totalQuestions={questions.length}
+          />
+      )
+    }
+
 
 
   return (
     <div className="flex flex-col flex-1">
         <div className="position-sticky top-0 z-10 shadow-md py-4 w-full">
             <header className="grid grid-cols-[auto,1fr,auto] grid-flow-col items-center justify-between py-2 gap-2">
-                <Button size="icon" variant="outline"><ChevronLeft/></Button>
+                <Button size="icon" variant={"outline"}><ChevronLeft/></Button>
                 <ProgressBar value={(currentQuestion/questions.length)*100}/>
                 <Button size="icon" variant="outline"><X/></Button>
 
@@ -98,8 +115,9 @@ export default function Home() {
             <div className="grid grid-cols-1 gap-6 mt-6">
                 {
                     questions[currentQuestion].answers.map(answer=>{
+                      const variant = selectedAnswer === answer.id ? (answer.isCorrect?"neoSuccess":"neoDanger"):"neoOutline";
                         return (
-                            <Button key={answer.id} variant={"secondary"} onClick={()=> handleAnswer(answer)}>{answer.answerText}</Button>
+                            <Button key={answer.id} variant={variant} size="xl" onClick={()=> handleAnswer(answer)}>{answer.answerText}</Button>
                         )
                     })
                 }
@@ -110,7 +128,7 @@ export default function Home() {
     </main>
     <footer className="footer pb-9 px-6 relative mb-0">
         <ResultCard isCorrect={isCorrect} correctAnswer={questions[currentQuestion].answers.find(answer => answer.isCorrect === true)?.answerText}/>
-        <Button variant="neoOutline" size="lg" onClick={handleNext}>{!started ? 'Start': 'Next'}</Button>
+        <Button variant="neoOutline" size="lg" onClick={handleNext}>{!started ? 'Start': (currentQuestion === questions.length-1)? 'Submit':'Next'}</Button>
     </footer>
     </div>
   )
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,10 +1,14 @@
-import * as React from "react"
-import { Slot } from "@radix-ui/react-slot"
-import { cva, type VariantProps,cx } from "class-variance-authority"
-import {clsx} from "clsx";
-import { cn } from "@/lib/utils"
+'use client';
 
-const neoClasses = "w-full rounded-full px-3.5 py-5 border-2 relative z-10 text-lg font-bold hover:transform hover:translate-y-[-2px] transition-transform-duration-200";
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps, cx } from "class-variance-authority";
+import { clsx } from "clsx";
+import { cn } from "@/lib/utils";
+
+const neoClasses =
+  "w-full rounded-full px-1 py-1 border-2 relative z-10 text-lg font-bold hover:transform hover:translate-y-[-2px] transition-transform duration-200";
+
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
@@ -12,21 +16,22 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline: "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        neo: cn(neoClasses,"bg-[#3e406a] text-white border-[#6366a7]"),
-        neoOutline: cn(neoClasses,"bg-[#3e406a] text-white border-[#6366a7]"),
+        neo: cn(neoClasses, "bg-primary text-primary-foreground border-blue-900"),
+        neoOutline: cn(neoClasses, "bg-[#3e406a] text-white border-[#6366a7]"),
+        neoSuccess: cn(neoClasses, "bg-green-500 text-primary-foreground border-green-600"),
+        neoDanger: cn(neoClasses, "bg-red-500 text-primary-foreground border-red-600"),
+
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-14 rounded-full px-3.5 py-5",
+        xl: "rounded-2xl h-26 px-6 py-3",
         icon: "h-10 w-10",
       },
     },
@@ -35,56 +40,59 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
 }
 
 export const spanVariants = cva(
-  ["absolute","h-14","bottom-[-7px]","w-full","border-2","left-0","z-0"],{
+  ["absolute", "h-14", "bottom-[-7px]", "w-full", "border-2", "left-0", "z-0"],
+  {
     variants: {
       variant: {
         default: "hidden",
-        destructive:
-          "hidden",
-        outline:
-          "hidden",
-        secondary:
-          "hidden",
+        destructive: "hidden",
+        outline: "hidden",
+        secondary: "hidden",
         ghost: "hidden",
         link: "hidden",
-        neo: ("border-blue-900 bg-primary-shadow"),
+        neo: "border-blue-900 bg-primary-shadow",
+        neoOutline: "bg-[#6366a7] border-[#6366f6]",
+        neoSuccess:"border-green-500 bg-green-600",
+        neoDanger:"border-red-500 bg-red-600"
       },
-  },
-  defaultVariants:{
-    variant:"default"
+    },
+    defaultVariants: {
+      variant: "default",
+    },
   }
-}
-)
+);
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
 
-    const showShadow = variant === "neo";
+    const showShadow = variant === "neo" || variant === "neoOutline" ||variant==="neoSuccess"||variant==="neoDanger";
+
+    const borderRound = clsx({
+      "rounded-full": variant === "neo",
+      "rounded-2xl": variant === "neoOutline" ||variant==="neoSuccess"||variant==="neoDanger",
+    });
 
     return (
       <div className={cn("relative inline-block w-full", { "pb-2": showShadow })}>
         {showShadow && (
           <span
-            className={cn(
-              "absolute bottom-0 left-0 w-full h-full rounded-full",
-              "border-2 border-blue-900 bg-primary-shadow z-0"
-            )}
+            className={cn(spanVariants({ variant }), borderRound)}
           />
         )}
 
         <Comp
           className={cn(
-            "relative z-10", // Ensures button stays above shadow
+            "relative z-10",
             buttonVariants({ variant, size, className })
           )}
           ref={ref}
@@ -95,6 +103,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   }
 );
 
-Button.displayName = "Button"
+Button.displayName = "Button";
 
-export { Button, buttonVariants }
+export { Button, buttonVariants };
